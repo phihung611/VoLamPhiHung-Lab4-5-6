1805542a0dadcced929de5be9b27a9e156338ce1
Administrator <Administrator@PC247>
2020-06-12 17:23:44 +0700

    Add project files.

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        [StringLength(225)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+
+    }
+}
---
+++ BigSchool/Models/StringLengthAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BigSchool.Models
+{
+    internal class StringLengthAttribute : Attribute
+    {
+        private int v;
+
+        public StringLengthAttribute(int v)
+        {
+            this.v = v;
+        }
+
+        public string ErrorMessage { get; set; }
+        public int MinimumLength { get; set; }
+    }
+}
